6382b7a6dca7109b4906097acde8c10c37a5ec80
Sammii7281 <sammiibarley@gmail.com>
2022-09-30 18:42:37 -0400

    intial commit

--- GitPractice.java
+++ GitPractice.java
@@ -1,8 +1,10 @@
-package GitPractice;
+package git;
 
 public class GitPractice {
 	public static void mai(String[] args) {
 		System.out.println("hello world");
+		System.out.print("FUCK THIS");
+		System.out.print("I DO NOT CONSENT");
 	}
 
 }
